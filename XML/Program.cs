7ff3f23a3f06c9fb98b326e16dbb726e78bbc337
lx <lx-1440731871@qq.com>
2021-11-30 21:53:51 +0800

    C# XML 操作

---
+++ XML/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace XML
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Test1();
+
+            Console.ReadLine();
+        }
+
+        public static void Test1()
+        {
+            // 创建对象集合
+            List<Student> list = new List<Student>();
+
+            // 【1】创建XML文档操作对象
+            XmlDocument objDoc = new XmlDocument();
+            // 【2】加载XML文件到文档对象中
+            objDoc.Load("Student.xml");
+            // 【3】获取XML文档根目录
+            XmlNode rootNode = objDoc.DocumentElement;
+            // 【4】遍历根节点（根节点包含所有节点）
+            foreach (XmlNode stuNode in rootNode.ChildNodes)
+            {
+                if (stuNode.Name == "Student")
+                {
+                    Student objStu = new Student();
+                    // 【5】遍历子节点
+                    foreach (XmlNode subNode in stuNode)
+                    {
+                        // 根据子节点的名称封装到对象的属性
+                        switch (subNode.Name)
+                        {
+                            case "StuName":
+                                objStu.StuName = subNode.InnerText; // 获取节点名称对应的节点值
+                                break;
+                            case "StuAge":
+                                objStu.StuAge = Convert.ToInt32(subNode.InnerText);
+                                break;
+                            case "Gender":
+                                objStu.Gender = Convert.ToInt32(subNode.InnerText);
+                                break;
+                            case "ClassName":
+                                objStu.ClassName = subNode.InnerText;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    list.Add(objStu);
+                }
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.StuName + "\t" + item.Gender + "\t" + item.StuAge + "\t" + item.ClassName);
+            }
+        }
+    }
+}
---
+++ XML/Student.cs
@@ -0,0 +1,25 @@
+﻿namespace XML
+{
+    public class Student
+    {
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string StuName { get; set; }
+
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        public int StuAge { get; set; }
+
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public int Gender { get; set; }
+
+        /// <summary>
+        /// 班级
+        /// </summary>
+        public string ClassName { get; set; }
+    }
+}
